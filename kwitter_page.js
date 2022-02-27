786d30bd125769d559cde946dd364b2a81e0be15
lidzard <90536035+lidzard@users.noreply.github.com>
2022-02-26 17:40:42 -0700

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,59 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+    apiKey: "AIzaSyDF6vez6qmfEUDi_1X6fNVQ-_CFSD0JNB4",
+    authDomain: "practice-60cc9.firebaseapp.com",
+    databaseURL: "https://practice-60cc9-default-rtdb.firebaseio.com",
+    projectId: "practice-60cc9",
+    storageBucket: "practice-60cc9.appspot.com",
+    messagingSenderId: "128418128890",
+    appId: "1:128418128890:web:35e32e8a6e6c1747823a43",
+    measurementId: "G-CVP598LXQ3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function getData() {
+    firebase.database().ref("/" + room_name).on('value', function (snapshot) {
+          document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                childKey = childSnapshot.key; childData = childSnapshot.val(); if (childKey != "purpose") {
+                      firebase_message_id = childKey;
+                      message_data = childData;
+                      //Start code
+                      console.log(firebase_message_id);
+                      console.log(message_data);
+                      name = message_data('name');
+                      like = message_data('message');
+                      name_with_tag = "<h4> " + name + "<img class='user_tick' src='tick.png'></h4>";
+                      like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+                      span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+
+                      row = name_with_tag + message_with_tag + like_button + span_with_tag;
+                      document.getElementById("output").innerHTML += row;
+                      //End code
+                }
+          });
+    });
+}
+getData();
+var room_name = localStorage.getItem("room_name");
+var user_name = localStorage.getItem("user_name");
+function send() {
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+          name: user_name,
+          message: msg,
+          like: 0
+    });
+    document.getElementById("msg").value = "";
+}
+function updateLike(message_id) {
+    console.log("clicked on like button - " + message_id);
+    button_id = message_id;
+    likes = document.getElementById(button_id).value;
+    updated_likes = Number(likes) + 1;
+    console.log(updated_likes);
+
+    firebase.database().ref(room_name).child(message_id).update({
+          like: updated_likes
+    });
+}
